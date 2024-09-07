8178c5b7f0c12684c12c185c94b873b61ad7b1ef
Ankur4600 <tinkushrivastav78@gmail.com>
2024-09-07 11:26:10 +0530

    variables

---
+++ basic/variable.js
@@ -0,0 +1,13 @@
+const customerId=988919
+let emailId="tinkushrivastav@gmail.com"
+var customerName="ankur"
+customerAddress="shyampatti"
+console.log(customerId);
+console.table([customerId, customerName,customerAddress,emailId])
+
+
+// customerId=7722
+emailId="ankur@gmail.com"
+customerName="piyush"
+customerAddress="tamkuhi raj"
+console.table([customerId, customerName,customerAddress,emailId])
