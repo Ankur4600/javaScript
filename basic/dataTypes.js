261edfcfef29d9567305688cb664670129ef837a
Ankur4600 <tinkushrivastav78@gmail.com>
2024-09-07 12:28:21 +0530

    dataType in script

---
+++ basic/dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" //treat all JS code as newer version
+
+
+
+
+//dataType
+let name = "ankur"
+let age = 22
+let isLoggedIn=true
+
+
+
+// number => 2 to power 53( bigint)
+//string => " "
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol=> unique
+
+
+
+
+//object
+console.log(typeof name) //string
+console.log(typeof null)// object
+console.log(typeof undefined)// undefined
