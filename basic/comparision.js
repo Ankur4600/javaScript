de4afe8183cb25d561aa7c85f8480df1a33e8905
Ankur4600 <tinkushrivastav78@gmail.com>
2024-09-08 12:16:20 +0530

    comparision

---
+++ basic/comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 == 1)
+// console.log(2 !=1)
+
+
+// console.log("2" > 1)//true
+// console.log("02">1)//true
+
+// console.log(null > 0)// false
+// console.log(null == 0)//false
+// console.log(null >= 0)//true
+
+// when we do type conversion while comparing  the output is not expected
+
+//what ever comparision operator you use it always gives false
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+
+// ===  also compare data type of  both values and avoid type conversion
+console.log("2" === 2)
+
