2ec9878c6751b0696b1f0cb13c098d9192070938
Ankur4600 <tinkushrivastav78@gmail.com>
2024-09-08 09:15:05 +0530

    conversion operation

---
+++ basic/conversionOperation.js
@@ -0,0 +1,35 @@
+let score ="ankur"
+// console.log(typeof (score))
+
+
+let scoreInNumber= Number(score)
+// console.log(typeof scoreInNumber)
+// console.log(scoreInNumber);
+
+
+//any other type of Number conversion
+//"44" => 44
+//"44abc"=> NaN
+//true => 1;
+//false => 0;
+
+
+
+let isloggedIn = "ankur";
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//any other type to boolean
+// 1 => true; 0 => false;
+// "" => false;
+//"ankur" => true;
+// 55 => true;
+
+
+
+//any other type to string
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
