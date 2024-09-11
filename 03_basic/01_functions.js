af2bf7e10356679953094de5e0bafaf1d7ad5a54
Ankur4600 <tinkushrivastav78@gmail.com>
2024-09-11 12:36:43 +0530

    parameters  in function

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -40,3 +40,43 @@ function loginUserMessage(userName = "ankur"){
 
 console.log(loginUserMessage()) //calling without arguument
 console.log(loginUserMessage("mohit")) // calling with argumnet 
+
+
+//rest operator (...)
+
+//with rest operator we an pass any number of arguments while calling the  function
+function calculateCartPrice( val1,val2, ...num){
+    return num
+}
+console.log(calculateCartPrice(200,500,999,0.00110))
+
+//passing object as the argument in the function and accesing its members
+const user ={
+    userName: "ankur",
+    age: 22
+}
+
+function passingObject(anyObject){
+    console.log(`username is ${anyObject.userName} and the age is ${anyObject.age}`)
+}
+passingObject(user)//passing object as argumnet
+
+
+//we can directly write object in the argumnet
+passingObject({
+    userName: "mohit",
+    age: 23
+})
+
+
+//we can also pass array as argunment
+const myNewArray = [9, 8, 8, 9, 1, 8, 7, 7, 2, 2]
+function returnThirdValue(array){
+    return array[2]
+}
+console.log(returnThirdValue(myNewArray))
+
+//we cxan also write array as argunment
+
+console.log(returnThirdValue([55,45,98,11,68]))
+
