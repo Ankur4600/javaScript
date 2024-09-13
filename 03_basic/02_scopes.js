92f14cb5d8c6f6d346fee8adfecc2bfd3488c517
Ankur4600 <tinkushrivastav78@gmail.com>
2024-09-13 08:33:22 +0530

    scope level

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -3,10 +3,53 @@ let a = 300
 if(true){
     let a = 10
     const b = 20
-    // console.log("INNER:", a)
+    console.log("INNER:", a)
 }
 
 
+
+
 // console.log(a);
-// console.log(b);
 // console.log(c);
+// console.log(b);
+
+
+function one(){
+    const userName = "ankur"
+    function two(){
+        const website = "youtube"
+        console.log(userName)
+    }
+    // console.log(website)
+
+    // two()
+}
+
+// one()
+
+if(true){
+    const userName ="ankur"
+    if(userName === "ankur"){
+        const website = " youtube"
+        console.log(userName + website)
+    }
+    //console.log(website)// throw error
+}
+
+//console.log(userName)//throw error
+
+//+
+
+addOne(5)
+function addOne(num){
+    return num+1
+}
+
+
+
+
+const addtwo = function(num){
+    return num + 2
+}
+addtwo(55)
+
