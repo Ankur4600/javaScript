76465a09f4824c032328dc53e136e5ae8f142141
Ankur4600 <tinkushrivastav78@gmail.com>
2024-09-15 12:19:22 +0530

    do while and while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+//while loop
+let index = 0
+while (index<=10) {
+  //  console.log(`Value of index is ${index}`)
+    index++;
+    
+}
+
+let arr =["Thor", "IronMan", "Hulk", "deadpool"]
+let i = 0
+while (i < arr.length) {
+   // console.log(arr[i]);
+    i=i+1;  
+}
+
+
+//do while loop
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`)
+    score++;
+} while (score <= 10);
