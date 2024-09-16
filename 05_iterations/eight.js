001616e729445e0477c105e0ce4939e7b83e3521
Ankur4600 <tinkushrivastav78@gmail.com>
2024-09-16 09:31:11 +0530

    reduce

---
+++ 05_iterations/eight.js
@@ -0,0 +1,38 @@
+//reduce
+const myNums = [1, 2, 3]
+
+// const mytotal = myNums.reduce(function (acc, currVal) {
+//     console.log(`acc: ${acc} and current value: ${currVal}`)
+//     return acc + currVal
+// }, 0)
+
+// console.log(mytotal)
+
+
+const myTotal = myNums.reduce( (acc, curr) => acc + curr, 0)
+console.log(myTotal)
+ 
+
+const shoppingCart = [
+    {
+        itemName: "js",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+console.log(priceToPay)
