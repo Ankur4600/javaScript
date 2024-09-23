c0246fb295264ea92736b1352eaa5db640c7e96c
Ankur4600 <tinkushrivastav78@gmail.com>
2024-09-23 13:05:52 +0530

    property Descriptor

---
+++ 09_classes_and_oop/mathsPi.js
@@ -0,0 +1,28 @@
+const descripter = Object.getOwnPropertyDescriptor(Math,"PI")
+console.log(descripter)
+console.log(Math.PI)
+
+const chai ={
+    name: 'ginger chai',
+    price: 150,
+    isAvailable: true,
+    orderChai: function(){
+        console.log('chai nhi banega')
+    }
+}
+console.log(Object.getOwnPropertyDescriptor(chai,"name"))
+Object.defineProperty(chai,'name',{
+    writable: false,
+    enumerable: false,
+    configurable: false
+})
+console.log(Object.getOwnPropertyDescriptor(chai,"name"))
+
+
+// console.log(chai.name)
+
+for (const [key,value] of Object.entries(chai)) {
+    if(typeof value !== 'function'){
+        console.log(`${key}: ${value}`)
+    }
+}
